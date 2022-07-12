8446dc70e7e3a82adc395897a2c31b7fca807e82
Wiktor Matecki <wmatecki97@gmail.com>
2022-07-12 22:17:08 +0200

    feat: Project files added

---
+++ Battleships.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Battleships.Tests
+{
+    public class UITests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckField_ValidField_RunsGameLogic()
+        {
+            var ui = new UI();
+
+        }
+    }
+}
---
+++ Battleships/GameLogic.cs
@@ -0,0 +1,17 @@
+﻿using Battleships.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    internal class GameLogic : IGameLogic
+    {
+        public bool CheckField(int x, int y)
+        {
+            return false;
+        }
+    }
+}
