86586af00d83f45175ca452d70b3c1d11182910e
Wiktor Matecki <wmatecki97@gmail.com>
2022-07-13 00:12:19 +0200

    test: Fire method tests added

--- Battleships.Tests/UITests.cs
+++ Battleships.Tests/UITests.cs
@@ -42,5 +42,63 @@ namespace Battleships.Tests
 
             logicMock.Verify(l => l.CheckField(It.IsAny<int>(), It.IsAny<int>()), Times.Never());
         }
+
+        [Test]
+        public void Fire_Hit_WritesHitMessage()
+        {
+            var logicMock = new Mock<IGameLogic>();
+            logicMock.Setup(l => l.CheckField(It.IsAny<int>(), It.IsAny<int>())).Returns(true);
+            logicMock.Setup(l => l.IsGameWon()).Returns(false);
+
+            var messegerMock = new Mock<IMessager>();
+            var inputTranslator = new Mock<IInputTranslator>();
+
+            var ui = new UI(logicMock.Object, messegerMock.Object, inputTranslator.Object);
+
+            ui.Fire(0,0,"player");
+
+            logicMock.Verify(l => l.CheckField(It.IsAny<int>(), It.IsAny<int>()), Times.Once());
+            messegerMock.Verify(m => m.Write(It.IsRegex(".*Hit.*")), Times.Once());
+        }
+
+        [Test]
+        public void Fire_Miss_WritesMissMessage()
+        {
+            var logicMock = new Mock<IGameLogic>();
+            logicMock.Setup(l => l.CheckField(It.IsAny<int>(), It.IsAny<int>())).Returns(false);
+            logicMock.Setup(l => l.IsGameWon()).Returns(false);
+
+            var messegerMock = new Mock<IMessager>();
+            var inputTranslator = new Mock<IInputTranslator>();
+
+            var ui = new UI(logicMock.Object, messegerMock.Object, inputTranslator.Object);
+
+            ui.Fire(0, 0, "player");
+
+            logicMock.Verify(l => l.CheckField(It.IsAny<int>(), It.IsAny<int>()), Times.Once());
+            messegerMock.Verify(m => m.Write(It.IsRegex("Miss")), Times.Once());
+            messegerMock.Verify(m => m.Write(It.IsAny<string>()), Times.Once());
+        }
+
+        [Test]
+        public void Fire_HitAdWon_WritesWonMessageWithPropperPlayerName()
+        {
+            var logicMock = new Mock<IGameLogic>();
+            logicMock.Setup(l => l.CheckField(It.IsAny<int>(), It.IsAny<int>())).Returns(true);
+            logicMock.Setup(l => l.IsGameWon()).Returns(true);
+
+            var messegerMock = new Mock<IMessager>();
+            var inputTranslator = new Mock<IInputTranslator>();
+
+            var ui = new UI(logicMock.Object, messegerMock.Object, inputTranslator.Object);
+
+            string player = "player";
+            ui.Fire(0, 0, player);
+
+            logicMock.Verify(l => l.CheckField(It.IsAny<int>(), It.IsAny<int>()), Times.Once());
+            messegerMock.Verify(m => m.Write(It.IsRegex("Hit")), Times.Once());
+            messegerMock.Verify(m => m.Write(It.IsRegex($"{player} won!")), Times.Once());
+            messegerMock.Verify(m => m.Write(It.IsAny<string>()), Times.Exactly(2));
+        }
     }
 }
