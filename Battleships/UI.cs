93f15262c7772e67ab977bdf1ee28f37bb956084
Wiktor Matecki <wmatecki97@gmail.com>
2022-07-12 23:13:02 +0200

    base flow added

--- Battleships/UI.cs
+++ Battleships/UI.cs
@@ -1,20 +1,57 @@
-﻿using Battleships.Interfaces;
+﻿using Battleships.Exceptions;
+using Battleships.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace Battleships
 {
     public class UI
     {
         private readonly IGameLogic logic;
+        private readonly IMessager messager;
+        private readonly IInputTranslator inputTranslator;
+        private bool _isRunning = true;
 
-        public UI(IGameLogic logic)
+        public UI(IGameLogic logic, IMessager messager, IInputTranslator inputTranslator)
         {
             this.logic = logic;
+            this.messager = messager;
+            this.inputTranslator = inputTranslator;
         }
 
-        public bool CheckField(string input)
+        public async void Run()
         {
-            //logic.CheckField(0, 0);
-            return false;
+            while (_isRunning)
+            {
+                ProcessNextRound();
+            }
+        }
+
+        public void ProcessNextRound()
+        {
+            try
+            {
+                string input = messager.GetInput();
+                (int x, int y) = inputTranslator.GetCoordinatesFromInput(input);
+
+                string playerName = "player";
+
+                var isHit = logic.CheckField(x, y); //todo struct with more info
+                var message = isHit ? "Hit!" : "Miss";
+                messager.Write(message);
+
+                var isGameWon = logic.IsGameWon();
+                if (isGameWon)
+                {
+                    messager.Write($"{playerName} won!");
+                    _isRunning = false;
+                }
+
+                //todo computer
+            }
+            catch (InvalidInputException)
+            {
+                messager.Write("Field coordinates should be a character followed by a number e.g. a1");
+            }
         }
     }
 }
